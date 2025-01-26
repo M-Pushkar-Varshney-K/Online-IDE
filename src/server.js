0025401589e9e42f903fb69d0a08e0f1ad2293b4
M.Pushkar Varshney.K <159184105+M-Pushkar-Varshney-K@users.noreply.github.com>
2025-01-26 15:34:57 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -17,8 +17,8 @@ app.post('/execute', async (req, res) => {
   try {
     // Send the request to JDoodle API
     const response = await axios.post('https://api.jdoodle.com/v1/execute', {
-      "clientId": "7e29164660439f58e6969ee26e3fff94", // Use your actual clientId
-      "clientSecret": "efc96759d2051a87b5c9b2c5c10b841bea4e4e1e1ec42046a9a27b72ba60218d", // Use your actual clientSecret
+      "clientId": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // Use your actual clientId
+      "clientSecret": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // Use your actual clientSecret
       "script": code, // The code sent by the client
       "stdin": "", // Optional: If you want to send input, you can modify this
       "language": lang, // The language specified by the client (e.g., "python3", "cpp")
